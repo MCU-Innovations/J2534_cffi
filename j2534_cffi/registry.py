d2e6bb29fcb014e459d15fc79ef3255225df687d
Ryan Hope <rmh3093@gmail.com>
2023-09-13 20:10:02 -0400

    winreg

--- j2534_cffi/registry.py
+++ j2534_cffi/registry.py
@@ -1,20 +1,23 @@
-import winreg
-
+try:
+    import winreg
+except:
+    winreg = None
 
 def __find_j2534_passthru_dlls(base_key):
     tool_info = []
     tool_list = []
     j2534_registry_info = []
     try:
-        count = winreg.QueryInfoKey(base_key)[0]
-        for idx in range(count):
-            device_key = winreg.OpenKeyEx(base_key, winreg.EnumKey(base_key, idx))
-            name = winreg.QueryValueEx(device_key, "Name")[0]
-            function_library = winreg.QueryValueEx(device_key, "FunctionLibrary")[0]
-            vendor = winreg.QueryValueEx(device_key, "Vendor")[0]
-            tool_list.append([name, function_library])
-            tool_info.append([idx, vendor, name, function_library])
-            j2534_registry_info.append(tool_info)
+        if winreg:
+            count = winreg.QueryInfoKey(base_key)[0]
+            for idx in range(count):
+                device_key = winreg.OpenKeyEx(base_key, winreg.EnumKey(base_key, idx))
+                name = winreg.QueryValueEx(device_key, "Name")[0]
+                function_library = winreg.QueryValueEx(device_key, "FunctionLibrary")[0]
+                vendor = winreg.QueryValueEx(device_key, "Vendor")[0]
+                tool_list.append([name, function_library])
+                tool_info.append([idx, vendor, name, function_library])
+                j2534_registry_info.append(tool_info)
     except Exception:
         pass
     return tool_list
@@ -22,12 +25,13 @@ def __find_j2534_passthru_dlls(base_key):
 
 def _find_j2534_passthru_dlls_wow6432():
     try:
-        base_key = winreg.OpenKeyEx(
-            winreg.HKEY_LOCAL_MACHINE,
-            r"Software\\WOW6432Node\\PassThruSupport.04.04\\",
-            access=winreg.KEY_READ | winreg.KEY_WOW64_64KEY,
-        )
-        return __find_j2534_passthru_dlls(base_key)
+        if winreg:
+            base_key = winreg.OpenKeyEx(
+                winreg.HKEY_LOCAL_MACHINE,
+                r"Software\\WOW6432Node\\PassThruSupport.04.04\\",
+                access=winreg.KEY_READ | winreg.KEY_WOW64_64KEY,
+            )
+            return __find_j2534_passthru_dlls(base_key)
     except Exception:
         pass
     return []
@@ -35,10 +39,11 @@ def _find_j2534_passthru_dlls_wow6432():
 
 def _find_j2534_passthru_dlls():
     try:
-        base_key = winreg.OpenKeyEx(
-            winreg.HKEY_LOCAL_MACHINE, r"Software\\PassThruSupport.04.04\\"
-        )
-        return __find_j2534_passthru_dlls(base_key)
+        if winreg:
+            base_key = winreg.OpenKeyEx(
+                winreg.HKEY_LOCAL_MACHINE, r"Software\\PassThruSupport.04.04\\"
+            )
+            return __find_j2534_passthru_dlls(base_key)
     except Exception:
         pass
     return []
