8a40acd97c7efef4698c888e14ceb65e1e3e2fd0
Ryan Hope <rmh3093@gmail.com>
2024-03-05 03:21:00 -0500

    fix ver

--- j2534_cffi/__init__.py
+++ j2534_cffi/__init__.py
@@ -1,3 +1,13 @@
-from ._version import version as __version__
+try:
+    from ._version import __VERSION__
+except:
+    import os
+    from setuptools_scm import get_version
+    def custom_version_scheme(version):
+        return f"{version.tag}.{version.distance}"
+    def custom_local_scheme(version):
+        return "+dirty" if version.dirty else ""
+    __VERSION__ = get_version(version_scheme=custom_version_scheme, local_scheme=custom_local_scheme, root=os.path.join(os.path.dirname(__file__), ".."))
+
 from .registry import find_j2534_passthru_dlls
 from .dll import J2534PassThru
