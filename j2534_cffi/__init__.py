c726496c7fb302943d6ee1177e3623c4bb3dbb05
Ryan Hope <rmh3093@gmail.com>
2024-03-13 18:05:18 -0400

    remove old version stuff

--- j2534_cffi/__init__.py
+++ j2534_cffi/__init__.py
@@ -1,13 +1,2 @@
-try:
-    from ._version import __version__ as __VERSION__
-except:
-    import os
-    from setuptools_scm import get_version
-    def custom_version_scheme(version):
-        return f"{version.tag}.{version.distance}"
-    def custom_local_scheme(version):
-        return "+dirty" if version.dirty else ""
-    __VERSION__ = get_version(version_scheme=custom_version_scheme, local_scheme=custom_local_scheme, root=os.path.join(os.path.dirname(__file__), ".."))
-
 from .registry import find_j2534_passthru_dlls
 from .dll import J2534PassThru
